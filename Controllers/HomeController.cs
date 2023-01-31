903989860ebc10942c6fb0473fa21ccc97facd04
Elvir Vlahovljak <elvirvlahovljak@gmail.com>
2023-01-31 21:24:53 +0100

    access to account with access token

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,34 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RedditOrganizeSaved.Models;
+using Reddit;
 
 namespace RedditOrganizeSaved.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        string appId = _configuration.GetValue<string>("Reddit:appId");
+        string appSecret = _configuration.GetValue<string>("Reddit:appSecret");
+        string accessToken = _configuration.GetValue<string>("Reddit:accessToken");
+        
+        RedditClient reddit = new RedditClient(appId, null, appSecret,
+            accessToken, "RedditOrganizer/0.1 by __rusmir__");
+
+        var spaseni = reddit.Account.Me.GetPostHistory("saved");
+        ViewBag.username = reddit.Account.Me.Name;
+
         return View();
     }
 
